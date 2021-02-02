f9ca12a0670ace8f41a905b0ca5f47aa362dcb05
HoussemGran <houssemgran1990@gmail.com>
2021-02-02 22:22:55 +0100

    client side

--- client.js
+++ client.js
@@ -4,3 +4,24 @@ const packageDef = protoLoader.loadSync("todo.proto" , {});
 const grpcObject = grpc.loadPackageDefinition(packageDef);
 const todoPackage = grpcObject.todoPackage;
 
+const text = process.argv[2];
+
+const client = todoPackage.Todo("localhost:3000" , 
+grpc.credentials.createInsecure()
+)
+
+client.createTodo({
+    "id": -1,
+    "text": text
+} , (err,res)=>{
+    console.log("received from server "+JSON.stringify(res));
+})
+
+
+const call = client.readTodosStream();
+
+call.on("data" , item=>{
+    console.log("received item from server "+ JSON.stringify(item))
+})
+
+call.on("end", e=> console.log("server done"))
