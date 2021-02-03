87696ea43a632cb8cbf7d5f07fd972e5abfd40db
HoussemGran <houssemgran1990@gmail.com>
2021-02-03 12:12:35 +0100

    gRPC

--- server.js
+++ server.js
@@ -1,48 +1,38 @@
-
 const grpc = require("grpc");
-const protoLoader = require("@grpc/proto-loader");
-const packageDef = protoLoader.loadSync("todo.proto" , {});
+const protoLoader = require("@grpc/proto-loader")
+const packageDef = protoLoader.loadSync("todo.proto", {});
 const grpcObject = grpc.loadPackageDefinition(packageDef);
 const todoPackage = grpcObject.todoPackage;
 
-
 const server = new grpc.Server();
+server.bind("0.0.0.0:3000",
+ grpc.ServerCredentials.createInsecure());
 
-server.bind("0.0.0.0:3000" , grpc.ServerCredentials.createInsecure());
-
-server.addService(todoPackage.Todo.service , {
-
-    "createTodo":createTodo,
-    "readTodos":readTodos,
-    "readTodosStream":readTodosStream
-
-})
-
+server.addService(todoPackage.Todo.service,
+    {
+        "createTodo": createTodo,
+        "readTodos" : readTodos,
+        "readTodosStream": readTodosStream
+    });
 server.start();
 
-const todos = [];
-
-function createTodo(call , callback){
-
+const todos = []
+function createTodo (call, callback) {
     const todoItem = {
-
-        "id":todos.length + 1,
-        "text":call.request.text
-
+        "id": todos.length + 1,
+        "text": call.request.text
     }
-
-    todos.push(todoItem);
-    callback(null , todoItem);
-
+    todos.push(todoItem)
+    callback(null, todoItem);
 }
 
-function readTodos(call , callback){
-
+function readTodosStream(call, callback) {
+    
     todos.forEach(t => call.write(t));
     call.end();
 }
 
-function readTodosStream(call , callback){
 
-    callback(null , {"items" : todos});
+function readTodos(call, callback) {
+    callback(null, {"items": todos})   
 }
